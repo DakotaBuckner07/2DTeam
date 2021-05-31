a02982304731e00d1c7c96dc1a1704d9f8c9159f
FreakieGamer101 <AWhitehouse@student.neumont.edu>
2021-05-31 14:46:51 -0600

    Started Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform start;
+    private Vector2 spawn;
+    private Vector2 position;
+    private bool isDead = false;
     void Start()
     {
-        
+        spawn.x = start.position.x;
+        spawn.y = start.position.y + 4;
+        position = spawn;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isDead)
+        {
+            position = spawn;
+            isDead = false;
+        }
     }
 }
