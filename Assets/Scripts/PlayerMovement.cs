5cd83171133ff0da22c5b8860e5186ea0d06cfc7
RemiixTaZe <Sirdracius926@gmail.com>
2021-05-31 18:05:14 -0600

    player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public CharacterController2D controller;
+    //public Animator anim;
+
+    [Range(0,100)]public float runSpeed = 40f;
+
+    float xMovement = 0f;
+    bool jump = false;
+
+    void Update()
+    {
+        xMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //controller.Move(xMovement * Time.fixedDeltaTime, jump);
+        if(jump)
+        {
+            //anim.SetTrigger("Jump");   
+        }
+        
+        jump = false;
+    }
+}
