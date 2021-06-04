be73db6bc7fdee085c2d29da6eb8b4e02d1c67e9
FreakieGamer101 <AWhitehouse@student.neumont.edu>
2021-06-04 14:11:29 -0600

    Hooked up audio

---
+++ Assets/SelectVolumn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SelectVolumn : MonoBehaviour
+{
+    public AudioMixerGroup mixer;
+    public void SetLevel(float sliderValue)
+    {
+        mixer.audioMixer.SetFloat(mixer.name, Mathf.Log10(sliderValue) * 20);
+    }
+}
