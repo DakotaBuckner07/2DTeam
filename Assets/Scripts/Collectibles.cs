1671dab5a9b8ff4f55105c01b4578fd45f4e52ab
RemiixTaZe <Sirdracius926@gmail.com>
2021-06-04 13:57:06 -0600

    Finishing touches

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.GetComponent<AudioSource>().Play();
+            gameObject.SetActive(false);
+            GameController.Instance.highScore += 1;
+        }
+    }
+}
